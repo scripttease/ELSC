fdd827f62480fe5ae78cafb5a8a88b136040bd0b
Alice Amy Dee <amyalicedee@gmail.com>
2016-01-29 08:30:14 +0000

    layout files and template added

--- app.rb
+++ app.rb
@@ -1,10 +1,23 @@
 require 'sinatra'
 
 get '/' do
+  @title = 'Welcome to ELSWC' 
   erb(:index)
 end
 get '/about' do
-  'All about world!'
+  @title = 'About the ELSW'
+end
+get '/people' do
+  "Meet the workers!"
+end
+get '/TsAndCs' do
+  "What you get from us and what we get from you"
+end
+get '/join' do
+  "Sign up (client)"
+end
+get '/contact' do
+  "Get in touch"
 end
 get '/profile/:name' do
   name = params[:name]
