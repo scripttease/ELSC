fad1c963d512387b735804e65eaa7b31581fabd9
Alice Amy Dee <amyalicedee@gmail.com>
2016-01-31 16:59:35 +0000

    converted sinatra app to modular application

--- app.rb
+++ app.rb
@@ -1,32 +1,41 @@
-require 'sinatra'
+require 'sinatra/base'
 
-get '/' do
-  @title = 'Welcome to ELSWC' 
-  erb(:index)
-end
-get '/about' do
-  @title = 'About the ELSW'
-  erb(:about)
-end
-get '/people' do
-  @title = 'Meet the workers!'
-  erb(:people)
-end
-get '/terms-and-conditions' do
-  @title = 'What you get from us and what we get from you'
-  erb(:terms_and_conditions)
-end
-get '/join' do
-  @title = 'Sign up (client)'
-  erb(:join)
-end
-get '/contact' do
-  @title = 'Get in touch'
-  erb(:contact)
-end
-get '/profile/:name' do
-  name = params[:name]
-  @title = '"Hello #{name.capitalize}"'
-  #erb(:profile)
-end
+class ELSWC < Sinatra::Application
 
+  get '/' do
+    @title = 'Welcome to ELSWC' 
+    erb(:index)
+  end
+
+  get '/about' do
+    @title = 'About the ELSW'
+    erb(:about)
+  end
+
+  get '/people' do
+    @title = 'Meet the workers!'
+    erb(:people)
+  end
+
+  get '/terms-and-conditions' do
+    @title = 'What you get from us and what we get from you'
+    erb(:terms_and_conditions)
+  end
+
+  get '/join' do
+    @title = 'Sign up (client)'
+    erb(:join)
+  end
+
+  get '/contact' do
+    @title = 'Get in touch'
+    erb(:contact)
+  end
+
+  get '/profile/:name' do
+    name = params[:name]
+    @title = '"Hello #{name.capitalize}"'
+    #erb(:profile)
+  end
+
+end
